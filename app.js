aac416b11b43ef9112d05d00009c92a41351a810
barbara figueiroa <bfamorim@outlook.com>
2021-07-15 19:55:34 -0300

    DEPLOY

--- app.js
+++ app.js
@@ -2,8 +2,9 @@
 const { Client}= require('discord.js')
 const botfunctions = require('./BotFunctions')
 const api = require("./api/StarWarsApi")
+require('dotenv/config')
 
 new Client()
 .on("message", botfunctions)
-.login('ODY0NjczNTMyODM5NTkxOTQ2.YO43-w.BxmhZo8Dv7_M0L3SE6mHOtizwLM');
+.login(process.env.APP_TOKEN);
 
